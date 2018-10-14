cd90314aef6a45f4bc400607dc9b1009838f444d
Finenka Artsiom <finn85@yandex.ru>
2018-10-14 22:54:08 +0300

    task 02_in progress(14/10/18)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'AngularPlatform';
+
+  userForm: FormGroup = new FormGroup({
+    'userName': new FormControl(null, [
+      Validators.required
+    ]),
+    'userAge': new FormControl(null, [
+      Validators.required
+    ]),
+    'userDateOfBirth': new FormControl(null, [
+      Validators.required
+    ]),
+    'userDateOfLogin': new FormControl(null, [
+      Validators.required
+    ]),
+    'userDateOfNotif': new FormControl(null, [
+      Validators.required
+    ])
+  });
+
+  placeholder = {
+    userName: 'Enter your name',
+    userAge: 'Enter your age',
+    userDateOfBirth: 'YYYY/MM/DD',
+    userDateOfLogin: 'DD MMMM YYYY',
+    userDateOfNotif: 'DD-MMM-YY'
+  };
+
+  submit = () => {
+    console.log(this.userForm);
+  }
 }
+
